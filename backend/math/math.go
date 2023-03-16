ac6f08f2aa722cb0c6ed7bd63679072c0007e363
Batuhan Özdemir <b.ozdmr23@gmail.com>
2023-03-16 19:44:59 +0300

    created simple pipeline

---
+++ backend/math/math.go
@@ -0,0 +1,17 @@
+package math
+
+func Add(a int, b int) int {
+	return 1
+}
+
+func Subtract(a int, b int) int {
+	return a - b
+}
+
+func Multiply(a int, b int) int {
+	return a * b
+}
+
+func Divide(a int, b int) float64 {
+	return float64(a / b)
+}
---
+++ backend/math/math_test.go
@@ -0,0 +1,13 @@
+package math_test
+
+import (
+	"github.com/batuhannoz/todo/math"
+	"testing"
+)
+
+func TestAdd(t *testing.T) {
+	result := math.Add(3, 2)
+	if result != 5 {
+		t.Errorf("Expected: %d Recevied: %d", 5, result)
+	}
+}
