5ce1935b99db09c9cbcbfd57a98115f7ba8c755e
Batuhan Özdemir <b.ozdmr23@gmail.com>
2023-03-17 16:30:35 +0300

    simple todo app components

---
+++ frontend/src/components/AddTodo.vue
@@ -0,0 +1,21 @@
+<template>
+  <input v-model="task"/>
+  <button @click="clicked"/>
+</template>
+
+<script>
+export default {
+  emits: ["addTask"],
+  data() {
+    return {
+      task: ""
+    }
+  },
+  methods: {
+    clicked() {
+      this.$emit('addTask', this.task)
+      this.task = ""
+    }
+  }
+}
+</script>
---
+++ frontend/src/components/TodoList.vue
@@ -0,0 +1,11 @@
+<template>
+  <li v-for="todo in todos" :key="todo.id">
+    {{ todo.task }}
+  </li>
+</template>
+
+<script>
+export default {
+  props: ['todos'],
+}
+</script>
